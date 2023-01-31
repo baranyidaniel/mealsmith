94084b825a63966cf67ae8d98acb0664e1c43846
IaamSteve <kerekesistvan@turr.hu>
2023-01-31 09:40:58 +0100

    angularjs isn't working for some reason, even chatgpt can't help lol

--- web/public/assets/js/script.js
+++ web/public/assets/js/script.js
@@ -1,5 +1,8 @@
-let app = new angular.module('recepturaApp', []);
+var app = new angular.module('recepturaApp', []);
 
 app.controller('mainCtrl', function($scope) {
-    $scope.title = "MealSmith";
-})
+    $scope.title = 'MealSmith';
+    $scope.author = 'RFG-HJK csapat';
+    $scope.company = 'Bajai SzC Türr István Technikum 2/14.SZFT';
+
+});
