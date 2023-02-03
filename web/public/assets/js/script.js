57e60d7972d886a4b32d357f1bc0d99632fa5151
vighakos <vighakos@turr.hu>
2023-02-03 09:37:07 +0100

    Routeprovider

--- web/public/assets/js/script.js
+++ web/public/assets/js/script.js
@@ -2,12 +2,42 @@ var app = new angular.module('mealsmithApp', ['ngRoute']);
 
 app.run(function($rootScope) {
 
-    $rootScope.settings = {};
     $rootScope.loggedUser = {};
     $rootScope.settings.appTitle = 'MealSmith';
     $rootScope.settings.company = 'Bajai SZC Türr István Technikum 2/14.SZFT';
-    $rootScope.settings.author = 'RFG-HJK csapat';
+    $rootScope.settings.author = 'VI-BA-KE csapat';
     $rootScope.loggedUser = angular.fromJson(sessionStorage.getItem('mealsmithApp'));
-
 });
-angular.bootstrap(document, ['mealsmithApp']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'receptCtrl'
+        })
+        .when('/reg', {
+            templateUrl: 'views/reg.html',
+            controller: 'userCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'userCtrl'
+        })
+        .when('/sajat', {
+            templateUrl: 'views/sajat.html',
+            controller: 'receptCtrl'
+        })
+        .when('/kedvencek', {
+            templateUrl: 'views/favorites.html',
+            controller: 'userCtrl'
+        })
+        .when('/koveteseim', {
+            templateUrl: 'views/follows.html',
+            controller: 'userCtrl'
+        })
+        .when('/top', {
+            templateUrl: 'views/top.html',
+            controller: 'userCtrl'
+        })
+        .otherwise('/')
+    });
