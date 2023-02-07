0ef3e58996218fcdf4a16439bf0415094c3d19e6
vighakos <vighakos@turr.hu>
2023-02-07 08:45:31 +0100

    receptCtrl selectAll

--- web/public/assets/js/dbFactory.js
+++ web/public/assets/js/dbFactory.js
@@ -2,6 +2,19 @@ app.factory('database', function($http, $q) {
     const url = 'http://localhost:5000'
 
     return {
+        selectAll: function(table) {
+            var deferred = $q.defer()
+            $http({
+                method: 'GET',
+                url: `${url}/${table}`
+            }).then(function(res) {
+                deferred.resolve(res)
+            }, function(res) {
+                deferred.reject(res)
+            })
+            return deferred.promise
+        },
+
         logincheck: function(data) {
             var deferred = $q.defer()
             $http({
