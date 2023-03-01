2803d007eb0d10722e9a8fcde4b13638598ff5bc
Baranyi Dániel <baranyidaniel@turr.hu>
2023-03-01 11:32:56 +0100

    Header módosítás, saját receptek oldal fejlesztések

--- web/public/controllers/profilCtrl.js
+++ web/public/controllers/profilCtrl.js
@@ -1,16 +1,13 @@
-app.controller('profilCtrl', function($scope, database, $rootScope, $location) {
+app.controller('profilCtrl', function($scope, database, $rootScope, $location, $routeParams, $filter) {
 
     $scope.user = {}
-    $scope.userRecipes = {}
-    
-    database.selectAll('users')
-    .then(function(res) {
-        $scope.user = res.data[0]
-    })
+    $scope.userRecipes = {}    
 
-    database.selectByValue('posts', 'id', '1')
-    .then(function(res) {
-        $scope.userRecipes = res.data
-        $scope.userRecipes = $filter('orderBy')($scope.receptek, '-points')
-    })
+    if ($routeParams.id != null){
+        database.selectByValue('posts', 'id', $routeParams.id)
+        .then(function(res) {
+            $scope.userRecipes = res.data
+            $scope.userRecipes = $filter('orderBy')($scope.receptek, '-points')
+        })
+    }
 })
