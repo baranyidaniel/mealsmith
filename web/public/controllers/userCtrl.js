cfef752f54b68aae8120c3087981ca23a72f6bbf
IaamSteve <kerekesistvan@turr.hu>
2023-03-03 12:30:50 +0100

    toplista funkciolnalítása

--- web/public/controllers/userCtrl.js
+++ web/public/controllers/userCtrl.js
@@ -1,75 +1,82 @@
-app.controller('userCtrl', function($scope, database, $rootScope, $location) {
+app.controller("userCtrl", function ($scope, database, $rootScope, $location) {
+  $scope.user = {};
 
-    $scope.user = {};
-
-    $scope.registration = function() {
-        if ($scope.user.username == null || $scope.user.email == null || $scope.user.pass1 == null || $scope.user.pass2 == null) {
-            alert('Nem adtál meg minden kötelező adatot!')
-            //return
-        }
-        if ($scope.user.pass1 != $scope.user.pass2) {
-            alert('A megadott jelszavak nem egyeznek!')
-            //return
-        }
-        else {
-            var pwd_pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,32}$/
-            if (!$scope.user.pass1.match(pwd_pattern)) {
-                alert('A megadott jelszó nem felel meg a minimális biztonsági követelményeknek!')
-                return
-            }
-        }
-        let data = {
-            name: $scope.user.name,
-            email: $scope.user.email,
-            passwd: CryptoJS.SHA1($scope.user.pass1).toString()
-        }
-
-        database.insert('users', data).then(function(res) {
-            if (res.data.affectedRows != 0) {
-                alert('A regisztráció sikeres! Beléphetsz az oldalra!');
-                $scope.user = {};
-            } else {
-                alert('Váratlan hiba történt az adatbázis művelet során!');
-            }
-        });
+  $scope.registration = function () {
+    if (
+      $scope.user.username == null ||
+      $scope.user.email == null ||
+      $scope.user.pass1 == null ||
+      $scope.user.pass2 == null
+    ) {
+      alert("Nem adtál meg minden kötelező adatot!");
+      //return
     }
+    if ($scope.user.pass1 != $scope.user.pass2) {
+      alert("A megadott jelszavak nem egyeznek!");
+      //return
+    } else {
+      var pwd_pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,32}$/;
+      if (!$scope.user.pass1.match(pwd_pattern)) {
+        alert(
+          "A megadott jelszó nem felel meg a minimális biztonsági követelményeknek!"
+        );
+        return;
+      }
+    }
+    let data = {
+      name: $scope.user.name,
+      email: $scope.user.email,
+      passwd: CryptoJS.SHA1($scope.user.pass1).toString(),
+    };
 
-    $scope.login = function() {
-        if ($scope.user.email == null || $scope.user.pass1 == null) {
-            alert('Nem adtál meg minden kötelező adatot!');
+    database.insert("users", data).then(function (res) {
+      if (res.data.affectedRows != 0) {
+        alert("A regisztráció sikeres! Beléphetsz az oldalra!");
+        $scope.user = {};
+      } else {
+        alert("Váratlan hiba történt az adatbázis művelet során!");
+      }
+    });
+  };
+
+  $scope.login = function () {
+    if ($scope.user.email == null || $scope.user.pass1 == null) {
+      alert("Nem adtál meg minden kötelező adatot!");
+    } else {
+      let data = {
+        table: "users",
+        email: $scope.user.email,
+        password: $scope.user.pass1,
+      };
+
+      database.logincheck(data).then(function (res) {
+        console.log(res.data);
+        if (res.data.length == 0) {
+          alert("Hibás belépési adatok!");
         } else {
+          if (res.data[0].status == 0) {
+            alert("Tiltott felhasználó!");
+          } else {
+            res.data[0].last = moment(new Date()).format("YYYY-MM-DD H:m:s");
+            $rootScope.loggedUser = res.data[0];
             let data = {
-                table: 'users',
-                email: $scope.user.email,
-                password: $scope.user.pass1
-            }
-
-            database.logincheck(data).then(function(res) {
-                console.log(res.data);
-                if (res.data.length == 0) {
-                    alert('Hibás belépési adatok!');
-                } else {
-                    if (res.data[0].status == 0) {
-                        alert('Tiltott felhasználó!');
-                    } else {
-
-                        res.data[0].last = moment(new Date()).format('YYYY-MM-DD H:m:s');
-                        $rootScope.loggedUser = res.data[0];
-                        let data = {
-                            last: res.data[0].last
-                        }
-                        database.update('users', res.data[0].ID, data).then(function(res) {
-                            sessionStorage.setItem('mealsmithApp', angular.toJson($rootScope.loggedUser));
-                        });
-                    }
-                }
+              last: res.data[0].last,
+            };
+            database.update("users", res.data[0].ID, data).then(function (res) {
+              sessionStorage.setItem(
+                "mealsmithApp",
+                angular.toJson($rootScope.loggedUser)
+              );
             });
+          }
         }
+      });
     }
+  };
 
-    $scope.logout = function() {
-        $rootScope.loggedUser = null;
-        sessionStorage.removeItem('mealsmithApp');
-        $location.path('/');
-    }
+  $scope.logout = function () {
+    $rootScope.loggedUser = null;
+    sessionStorage.removeItem("mealsmithApp");
+    $location.path("/");
+  };
 });
