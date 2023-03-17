e97c3c521c6cc43281f466b16d83b7a10d5fd360
vighakos <vighakos@turr.hu>
2023-03-17 12:26:42 +0100

    kedvencek

--- web/public/controllers/kedvencekCtrl.js
+++ web/public/controllers/kedvencekCtrl.js
@@ -1,11 +1,17 @@
 app.controller('kedvencekCtrl', function($scope, $rootScope, database, $location, $filter) {
     $scope.receptek = []
 
-    database.selectByValue('favorites', 'user_id', $rootScope.loggedUser.id)
-    .then(function(res) {
-        $scope.receptek = res.data;
-        //$scope.receptek = $filter('orderBy')($scope.receptek, '-points')
-    })
+    $scope.feltolt = function() {
+        database.selectByValue('favorites', 'user_id', $rootScope.loggedUser.id)
+        .then(function(res) {
+            let favorites = res.data;
+            favorites.forEach(item => {
+                database.selectByValue('posts', 'id', item.post_id).then(function(res) {
+                    $scope.receptek.push(res.data[0])
+                })
+            })
+        })
+    }
 
     $scope.elkeszites = function(id) {
         let idx = $scope.receptek.findIndex(item => item.id === id);
@@ -22,5 +28,26 @@ app.controller('kedvencekCtrl', function($scope, $rootScope, database, $location
 
     $scope.showRecept = function(id) {
         $location.path('/receptek/' + id)
+    }
+
+    $scope.addToFavorites = function(id) {
+        database.selectByValue('favorites', 'user_id', $rootScope.loggedUser.id)
+            .then(function(res) {
+                let tomb = res.data
+                tomb.forEach(item => {
+                    if (item.post_id == id) {
+                        database.delete('favorites', 'post_id', id)
+                        return
+                    }
+                })
+                
+                let data = {
+                    user_id: $rootScope.loggedUser.id,
+                    post_id: id
+                }
+
+                database.insert('favorites', data)
+            }
+        )
     }
 });
