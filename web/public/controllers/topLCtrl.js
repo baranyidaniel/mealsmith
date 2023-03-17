6b4c600e5658f9bea6d1ff09a905fa09dad2c242
IaamSteve <kerekesistvan@turr.hu>
2023-03-17 12:22:07 +0100

    toplista feltöltése

--- web/public/controllers/topLCtrl.js
+++ web/public/controllers/topLCtrl.js
@@ -1,5 +1,14 @@
 app.controller("topLCtrl", function ($scope, database, $rootScope, $location) {
-  $scope.user = {};
+  $scope.userPoints = {};
+  $scope.users = {};
+  $scope.db = [];
+  database.selectAll("users").then(function (res) {
+    $scope.users = res.data;
+    for (let i = 0; i < $scope.users.length; i++) {
+      $scope.db[i] = 1;
+    }
+  });
+
   $scope.fill = function () {
     let data = {
       table: "users",
