14c43bd003849ef9d43f744e9317ba1ea303e12b
Baranyi Dániel <baranyidaniel@turr.hu>
2023-03-20 13:20:33 +0100

    Kártya controll elkészítve

---
+++ web/public/controllers/kartyaCtrl.js
@@ -0,0 +1,57 @@
+app.controller('kartyaCtrl', function($scope, $rootScope, database, $location, $filter) {
+    $scope.receptek = []
+
+    database.selectAll('posts')
+    .then(function(res) {
+        $scope.receptek = res.data;
+        $scope.receptek = $filter('orderBy')($scope.receptek, '-points')
+    })
+
+    $scope.elkeszites = function(id) {
+        let idx = $scope.receptek.findIndex(item => item.id === id);
+
+        let ora = Math.floor($scope.receptek[idx].elkeszitesi_ido / 60)
+        let perc = $scope.receptek[idx].elkeszitesi_ido % 60
+
+        return `${ora != 0 ? ora + " óra" : ""} ${perc != 0 ? perc + " perc" : ""}`
+    }
+
+    $scope.ido = function(id) {
+        moment.locale("hu")
+        let idx = $scope.receptek.findIndex(item => item.id === id)
+        return moment($scope.receptek[idx].datum, "YYYYMMDD").fromNow()
+    }
+
+    $scope.orderByLatest = function() {
+        $scope.receptek = $filter('orderBy')($scope.receptek, '-datum')
+    }
+
+    $scope.orderByPoints = function() {
+        $scope.receptek = $filter('orderBy')($scope.receptek, '-points')
+    }
+
+    $scope.showRecept = function(id) {
+        $location.path('/receptek/' + id)
+    }
+
+    $scope.addToFavorites = function(id) {
+        database.selectByValue('favorites', 'user_id', $rootScope.loggedUser.id)
+            .then(function(res) {
+                let tomb = res.data
+                tomb.forEach(item => {
+                    if (item.post_id == id) {
+                        database.delete('favorites', 'post_id', id)
+                        return
+                    }
+                })
+                
+                let data = {
+                    user_id: $rootScope.loggedUser.id,
+                    post_id: id
+                }
+
+                database.insert('favorites', data)
+            }
+        )
+    }
+});
