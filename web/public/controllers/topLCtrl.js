ddb4baa38bd0f38aa0c9cd329b99e248be23bb35
IaamSteve <kerekesistvan@turr.hu>
2023-03-22 11:36:02 +0100

    toplista kártyák

--- web/public/controllers/topLCtrl.js
+++ web/public/controllers/topLCtrl.js
@@ -1,11 +1,13 @@
   app.controller("topLCtrl", function ($scope, database, $rootScope, $location) {
-    $scope.tops =   [];
+    $scope.tops = [];
   database.selectAll("users", {}).then(function (res) {
     $scope.tops =res.data;
     for (let i = 0; i < res.length; i++) {
       $scope.top = {
+        userID: res[i].id,
         username: res[i].username,
-        points: res[i].points
+        points: res[i].points,
+        img:res[i].img
       };
       $scope.tops.push($scope.top);
       console.log($scope.tops);
@@ -14,4 +16,19 @@
   }).catch(function (err) {
     console.error(err);
   });
+  //Alább a postok száma lesz meghatározva
+  /*
+  database.selectAll("posts", {}).then(function (res) {
+    $scope.tops =res.data;
+    for (let i = 0; i < res.length; i++) {
+      $scope.top = {
+       posts:res[i].
+      };
+      $scope.tops.push($scope.top);
+      console.log($scope.tops);
+    }
+   
+  }).catch(function (err) {
+    console.error(err);
+  });*/
 });
