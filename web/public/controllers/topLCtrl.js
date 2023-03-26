1be0bac54e2edd2ab6e1ffa5d2a29da272eb5100
Kerekes István <kerekesistvan@turr.hu>
2023-03-26 22:32:17 +0200

    Toplista oldal postok lekérése

--- web/public/controllers/topLCtrl.js
+++ web/public/controllers/topLCtrl.js
@@ -1,34 +1,32 @@
-  app.controller("topLCtrl", function ($scope, database, $rootScope, $location) {
+app.controller("topLCtrl", function($scope, database, $rootScope, $location) {
+    var query =
+        "SELECT users.*, COUNT(posts.id) AS numOfPosts FROM users LEFT JOIN posts ON users.id = posts.user_id GROUP BY users.id";
     $scope.tops = [];
-  database.selectAll("users", {}).then(function (res) {
-    $scope.tops =res.data;
-    for (let i = 0; i < res.length; i++) {
-      $scope.top = {
-        userID: res[i].id,
-        username: res[i].username,
-        points: res[i].points,
-        img:res[i].img
-      };
-      $scope.tops.push($scope.top);
-      console.log($scope.tops);
-    }
-   
-  }).catch(function (err) {
-    console.error(err);
-  });
-  //Alább a postok száma lesz meghatározva
-  /*
-  database.selectAll("posts", {}).then(function (res) {
-    $scope.tops =res.data;
-    for (let i = 0; i < res.length; i++) {
-      $scope.top = {
-       posts:res[i].
-      };
-      $scope.tops.push($scope.top);
-      console.log($scope.tops);
-    }
-   
-  }).catch(function (err) {
-    console.error(err);
-  });*/
+    $scope.posts = [];
+    database
+        .query(query)
+        .then(function(res) {
+            $scope.posts = res.data;
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+    database
+        .selectAll("users", {})
+        .then(function(res) {
+            $scope.tops = res.data;
+            for (let i = 0; i < res.length; i++) {
+                $scope.top = {
+                    userID: res[i].id,
+                    username: res[i].username,
+                    points: res[i].points,
+                    img: res[i].img,
+                };
+                $scope.tops.push($scope.top);
+                console.log($scope.tops);
+            }
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
 });
