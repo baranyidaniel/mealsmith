2e56e50a454ccd8b2b44dfd2bd86d9ba4e8721bc
IaamSteve <kerekesistvan@turr.hu>
2023-03-28 09:39:53 +0200

    SQL Syntax Error nem lett megoldva

--- web/public/controllers/topLCtrl.js
+++ web/public/controllers/topLCtrl.js
@@ -1,33 +1,34 @@
 app.controller("topLCtrl", function ($scope, database, $rootScope, $location) { 
    $scope.posts = [];
     database.select("posts", {}).then(function (res) {
-        /*database.query("SELECT COUNT(posts.id) AS numOfPosts FROM users GROUP BY users.id")*/
         $scope.posts= res.data;
         for (let i = 0; i < res.length; i++) {
           $scope.post = {
-            
+            id: res[i].id,
+            user_id: res[i].user_id,
           };
+          $scope.posts.push($scope.post);
+          console.log($scope.posts.length);
         } 
+    }).catch(function (err) {
+        console.log(err);
     });
-
 
     $scope.tops = [];
     database.selectAll("users", {}).then(function (res) {
         $scope.tops = res.data;
         for (let i = 0; i < res.length; i++) {
             $scope.top = {
-                userID: res[i].id,
+                id: res[i].id,
                 username: res[i].username,
                 points: res[i].points,
                 img: res[i].img,
                 posts: 0
             };
             $scope.tops.push($scope.top);
-            //$scope.tops[i].posts = $scope.points[i].numOfPosts;
             console.log($scope.tops);
         }
     }).catch(function (err) {
-        console.error(err);
+        console.log(err);
     });
-    
 });
