4841654086dc9c07fe04deaf41644e415440debb
IaamSteve <kerekesistvan@turr.hu>
2023-03-29 11:34:20 +0200

    Posztok mennyiségének lekérése szűrés nélkül

--- web/public/controllers/topLCtrl.js
+++ web/public/controllers/topLCtrl.js
@@ -1,18 +1,25 @@
 app.controller("topLCtrl", function ($scope, database, $rootScope, $location) { 
-   $scope.posts = [];
-    database.select("posts", {}).then(function (res) {
-        $scope.posts= res.data;
+
+    $scope.posts = [];
+    database.selectAll("posts", {}).then(function (res) {
+        $scope.posts = res.data;
         for (let i = 0; i < res.length; i++) {
-          $scope.post = {
-            id: res[i].id,
-            user_id: res[i].user_id,
-          };
-          $scope.posts.push($scope.post);
-          console.log($scope.posts.length);
-        } 
+            $scope.post = {
+                id: res[i].id,
+                uid: res[i].userid
+            };
+            var tempArr = ($scope.posts).filter(function(d){
+                return (d.select == uid);
+           });
+           console.log(tempArr);
+            $scope.posts.push($scope.post);
+            console.log($scope.posts);
+        }
     }).catch(function (err) {
         console.log(err);
     });
+
+
 
     $scope.tops = [];
     database.selectAll("users", {}).then(function (res) {
@@ -23,7 +30,6 @@ app.controller("topLCtrl", function ($scope, database, $rootScope, $location) {
                 username: res[i].username,
                 points: res[i].points,
                 img: res[i].img,
-                posts: 0
             };
             $scope.tops.push($scope.top);
             console.log($scope.tops);
