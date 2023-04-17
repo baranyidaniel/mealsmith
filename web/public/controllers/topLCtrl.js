83fad4b6348233a8c7a15f0ce59a787f97727efe
IaamSteve <kerekesistvan@turr.hu>
2023-04-17 09:37:54 +0200

    toplista top3 és maradék külön szedése - fail

--- web/public/controllers/topLCtrl.js
+++ web/public/controllers/topLCtrl.js
@@ -1,26 +1,32 @@
-app.controller("topLCtrl", function ($scope, database, $rootScope, $location) { 
+app.controller("topLCtrl", function ($scope, database) {
+  $scope.posts = [];
+  $scope.top3 = [];
+  $scope.tops = [];
 
-    $scope.posts = [];
-    $scope.tops = [];
+  database.selectAll("posts").then(function (res) {
+    $scope.posts = res.data;
 
-    database.selectAll("posts").then(function (res) {
-        $scope.posts = res.data;
+    database.selectAll("users").then(function (res) {
+      $scope.tops = res.data;
+      $scope.tops.forEach((item) => {
+        item.posts = $scope.getUserPosts(item.id);
+      });
+    });
+  });
 
-        database.selectAll("users").then(function (res) {
-            $scope.tops = res.data
-            $scope.tops.forEach(item => {
-                item.posts = $scope.getUserPosts(item.id)
-            })
-        })
-    })
+  $scope.getUserPosts = function (id) {
+    let num = 0;
 
-    $scope.getUserPosts = function(id) {
-        let num = 0;
+    $scope.posts.forEach((item) => {
+      if (item.user_id == id) num++;
+    });
 
-        $scope.posts.forEach(item => {
-            if (item.user_id == id) num++
-        });
+    return num;
+  };
 
-        return num;
-    }
+    $scope.tops.sort(function (a, b){
+        return b.points - a.points;
+    });
+    $scope.top3.push.apply($scope.top3, $scope.tops.slice(0, 3));
+    $scope.tops.splice(0, 3);
 });
